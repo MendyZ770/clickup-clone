c7ca476276d09a77a3021482c85b42bdb20ebe0e
Fleischer <Mendyzerbib1606@gmail.com>
2026-07-07 20:23:50 +0200

    style: lower mobile nav pill and enable android immersive mode

--- android/app/src/main/java/com/done/app/MainActivity.java
+++ android/app/src/main/java/com/done/app/MainActivity.java
@@ -1,5 +1,6 @@
 package com.done.app;
 
+import android.view.View;
 import android.os.Bundle;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
@@ -14,5 +15,26 @@ public class MainActivity extends BridgeActivity {
     WebView webView = this.getBridge().getWebView();
     webView.clearCache(true);
     webView.getSettings().setCacheMode(android.webkit.WebSettings.LOAD_NO_CACHE);
+
+    hideSystemUI();
+  }
+
+  @Override
+  public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+        hideSystemUI();
+    }
+  }
+
+  private void hideSystemUI() {
+    View decorView = getWindow().getDecorView();
+    decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN);
   }
 }
